c7e624d699fef8b60753e96bf538d177375e205e
Manyathaaa <mailtomanyatha@gmail.com>
2025-08-03 20:09:17 +0530

    database connected

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
+connectDB();
 
+app.use(express.json());
 const PORT = 1011;
 
 app.listen(PORT, () => {
